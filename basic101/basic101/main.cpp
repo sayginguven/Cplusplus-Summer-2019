7508b85fed59c760586b0f88bca7aa1c8be7291b
sayginguven <sayginguven@yahoo.com>
2019-07-29 09:08:20 -0700

    initial main

---
+++ basic101/basic101/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  basic101
+//
+//  Created by Saygin Guven on 2019-07-29.
+//  Copyright © 2019 Saygin Guven. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
