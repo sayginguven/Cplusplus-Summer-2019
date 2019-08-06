a86fb5530cbb81bf104904da810bc22ae6509fc5
sayginguven <sayginguven@yahoo.com>
2019-08-06 12:02:18 -0700

    class Cat created with constructors, copy constructor, destructor,  getters, setters, additional methods, and driver on main

--- basic101/basic101/main.cpp
+++ basic101/basic101/main.cpp
@@ -11,11 +11,45 @@
 #include "functions.hpp"
 #include "moreFunctions.hpp"
 #include <vector>
+#include <ctype.h>
 
+#include "../OOP/Cat.hpp"
 
 int main() {
     
+    if(1==1){
+        //default constructor will be called
+        Cat delegatedCat{};
+        delegatedCat.introduceYourself();
+        delegatedCat.jump();
+        
+    }
     
+    std::cout << "_____________" << std::endl;
+    Cat fluffy("Fluffy", "Black and white", false, 10.4, 100);
+    fluffy.introduceYourself();
+    fluffy.eat();
+
+    
+    std::cout << "_____________" << std::endl;
+    //C++11 syntax
+    Cat sylvester {"Sylvester","White",true, 5.4,95};
+    sylvester.introduceYourself();
+    sylvester.sleep();
+   
+    std::cout << "_____________" << std::endl;
+    Cat kit{"Homer", 100 };
+    kit.introduceYourself();
+    kit.eat();
+    kit.jump();
+    
+    std::cout << "_____________" << std::endl;
+    Cat copyFluffy(fluffy);
+    copyFluffy.introduceYourself();
+    
+    
+    std::cout << "_____________" << std::endl;
+
     return 0;
 }
 
