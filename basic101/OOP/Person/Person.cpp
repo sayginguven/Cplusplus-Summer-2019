1094f5dee5eb85e511f0f45dacdbd8c07fb7419c
sayginguven <sayginguven@yahoo.com>
2019-08-14 10:20:24 -0700

    created account and person classes headers without implementation

---
+++ basic101/OOP/Person/Person.cpp
@@ -0,0 +1,9 @@
+//
+//  Person.cpp
+//  basic101
+//
+//  Created by Saygin Guven on 2019-08-14.
+//  Copyright © 2019 Saygin Guven. All rights reserved.
+//
+
+#include "Person.hpp"
--- basic101/basic101/main.cpp
+++ basic101/basic101/main.cpp
@@ -10,10 +10,23 @@
 #include "moreFunctions.hpp"
 #include "../OOP/Time.hpp"
 #include "../OOP/Employee.hpp"
-
+#include "../OOP/Account/Account.hpp"
+#include "../OOP/Person/Person.hpp"
 
 int main() {
     
+    Person a;
     
-     return 0;
+    //Account(unsigned int number, int type[], double balance[], int branchNumber, Person holder);
+    
+    int typeArr[3] {1,2,3};
+    double balanceArr[3] {1000,0,0};
+    
+    //Account acc1(1,typeArr, new double[3] {1000,0,0},1,a);
+    
+    //a checking 1000 CAD
+    //a saving ?
+    //a master ?
+    
+    return 0;
 }
